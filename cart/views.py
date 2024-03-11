b1e29232642440ddb3b90709b46998875474ea32
Yogeshb42 <yogeshbhagat4242@gmail.com>
2024-03-12 02:03:45 +0530

    Cart functionality with session id

---
+++ cart/views.py
@@ -0,0 +1,76 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from store.models import Product
+from .models import Cart, CartItem
+
+
+def _create_cart(request):
+    try:
+        cart_id = request.session.session_key
+    except:
+        cart_id = request.session.create()
+    return cart_id
+
+
+def add_to_cart(request, product_id):
+    product = Product.objects.get(id=product_id)
+
+    try:
+        cart = Cart.objects.get(cart_id=_create_cart(request))
+    except :
+        cart = Cart.objects.create(cart_id=_create_cart(request))
+    cart.save()
+
+    try:
+        cart_item = CartItem.objects.get(product=product, cart=cart)
+        cart_item.quantity += 1
+    except:
+        cart_item = CartItem.objects.create(
+            product = product,
+            cart = cart,
+            quantity = 1,
+        )
+    cart_item.save()
+    print(cart_item.product)
+    return redirect('cart')
+
+
+def remove_from_cart(request, product_id):
+    product = get_object_or_404(Product, id=product_id)
+    cart = Cart.objects.get(cart_id=_create_cart(request))
+    cart_item = CartItem.objects.get(product=product, cart=cart)
+    if cart_item.quantity > 1:
+        cart_item.quantity -= 1
+        cart_item.save()
+    else: 
+        cart_item.delete()
+    
+    return redirect('cart')
+
+
+def remove_cart_item(request, product_id):
+    product = get_object_or_404(Product, id=product_id)
+    cart = Cart.objects.get(cart_id=_create_cart(request))
+    cart_item = CartItem.objects.get(product=product, cart=cart)
+    cart_item.delete()
+    return redirect('cart')
+
+
+def cart(request, total=0, quantity=0, cart_items=None):
+    try:
+        cart = Cart.objects.get(cart_id=_create_cart(request))
+        cart_items = CartItem.objects.filter(cart=cart, is_active=True)
+        for cart_item in cart_items:
+            total += (cart_item.product.price * cart_item.quantity)
+            quantity +=1
+        tax = (3 * total)/100
+        grand_total = total + tax
+    except:
+        pass
+    context = {
+        'cart_items': cart_items,
+        'total': total,
+        'quantity': quantity,
+        'tax': tax,
+        'grand_total': grand_total,
+    }
+    return render(request, 'cart/cart.html', context)
